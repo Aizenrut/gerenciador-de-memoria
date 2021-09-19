1ec235e176a0af2a0ac6334e2100ccbee228f724
Aizenrut <igorce2000@gmail.com>
2021-09-19 18:34:00 -0300

    Implementação do alocador

--- alocador.c
+++ alocador.c
@@ -1,8 +1,18 @@
 #include "alocador.h"
 
-MEMORIA_4KB = 4096;
+const int MEMORIA_4KB = 4096;
 
-char* memoria = NULL;
+typedef struct posicao
+{
+    char *ponteiro;
+    int indice;
+    int tamanho;
+    struct posicao *proxima;
+} posicao;
+
+posicao *inicio;
+
+char *memoria = NULL;
 
 int inicia_alocador()
 {
@@ -16,21 +26,82 @@ int inicia_alocador()
 
 int finalizar()
 {
-    if (munmap(memoria, MEMORIA_4KB) == -1)
+    if (memoria == NULL || munmap(memoria, MEMORIA_4KB) == -1)
        return 1;
     else
        return 0;
 }
 
-char *aloca(int)
+char *aloca(int quantidade)
 {
+    if (memoria == NULL)
+        return NULL;
+
+    int indice = 0;
+
+    if (inicio == NULL)
+    {
+        posicao *nova = { memoria[indice], indice, quantidade, NULL };
+        inicio = nova;
+        
+        return nova->ponteiro;
+    }
+    else
+    {
+        posicao *atual = inicio;
+        posicao *proxima;
+
+        while (atual->proxima != NULL)
+        {
+            proxima = atual->proxima;
+
+            if (atual->indice + atual->tamanho != atual->proxima->indice && atual->proxima->indice - atual->indice + atual->tamanho >= quantidade)
+            {
+                indice = atual->indice + atual->tamanho;
+                break;
+            }
+
+            atual = atual->proxima;
+        }
+        
+        posicao *nova = { memoria[indice], indice, quantidade, NULL };
+        
+        nova->proxima = proxima;
+        atual->proxima = nova;
+
+        return nova->ponteiro;
+    }
+
     return NULL;
 }
 
-int desaloca(char *)
+int desaloca(char * ponteiro)
 {
-    if (munmap(char*, tamanho) == -1)
+    if (inicio == NULL)
         return 1;
+
+    posicao *anterior;
+    posicao *atual = inicio;
+    posicao *proxima;
+
+    while (atual->proxima != NULL)
+    {
+        proxima = atual->proxima;
+
+        if (atual->ponteiro == ponteiro)
+            break;
+        
+        anterior = atual;
+        atual = proxima;
+    };
+        
+    if (munmap(ponteiro, atual->tamanho) == -1)
+        return 1;
+
+    if (anterior != NULL)
+        anterior->proxima = proxima;
     else
-        return 0;
+        inicio = proxima;
+
+    return 0;
 }
